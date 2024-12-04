b5d609a2c98020e326466ee9736a6c5ec54550f1
gabri@HomePC <gabriribeiro5@live.com>
2024-12-04 09:02:50 -0300

    style(out_of_process): Auth and Sessions will be designed as microservices. The out_of_process directory is a temporary representation

---
+++ src/purePython/out_of_process/auth.py
@@ -0,0 +1,26 @@
+import uuid
+from datetime import datetime, timedelta
+import threading
+import time
+from utils.definitions import SESSION_GROUPS_AND_TIMEOUTS
+from sessions import SessionManager as Sessions
+
+AUTH_TOKENS = {"client_address": "my_secret_token", "other_client_address": "not_my_token"}
+
+class AuthManager(Sessions):
+    '''
+    - Quem és tú, Jaburu?
+    - Atribuir label (Customer, Unit, etc)
+    - Solicitar criação de token
+    - Retorna o token
+    '''
+    def __init__(self, *args, **kwargs) -> None:
+        Sessions.__init__(self, *args, **kwargs)
+
+    # Verifica se a requisição está autenticada
+    def is_authenticated(self):
+        auth_header = self.headers.get("Authorization")
+        if auth_header and auth_header.startswith("Bearer "):
+            token = auth_header.split(" ")[1]
+            return token == AUTH_TOKENS[self.client_address]
+        return False
