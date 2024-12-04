7fef77fd391b885a8a06f041f204079ce146eb80
Gabriel Ribeiro <gabriribeiro5@live.com>
2024-12-04 15:23:03 -0300

    fix(logSetUp): make it work then and some try/except statements

--- src/purePython/utils/logSetUp.py
+++ src/purePython/utils/logSetUp.py
@@ -1,68 +1,63 @@
-import pdb
 # Dealing with ImportError: attempted relative import with no known parent package
 import sys
 sys.path.append('.')
 
 # logging setup
-# import utils.fileManager as fileManager
-# import os
 import logging
 import inspect
+from pathlib import Path
 
 def enableLog(dirName:str=".", logFileName:str="logfile"):
     """
     dirName: where must the lofile be saved?
     logFileName: what is the logfile name (usually the application name)?
     """
-
-    # fileName = fileManager.loadConfigFile()
-    # logFilePathAndName = os.path.join(os.environ['LOG_DIR'], fileName['logFile'])
-
-    #remove .py from appName
+    # remove .py from appName then normalize variables into paths
     if ".py" in logFileName:
         logFileName = logFileName[0:(len(logFileName)-3)]
-
-    print(logFileName)
-    pdb.set_trace()
-    fileName = f"{logFileName}.log"
-    pdb.set_trace()
-    logging.basicConfig(filename="test.log", encoding='utf-8', level=logging.DEBUG)
-    logging.basicConfig(filename=fileName, encoding='utf-8', level=logging.DEBUG)
-    pdb.set_trace()
-    logging.debug('This message should go to the log file')
-    pdb.set_trace()
-    logging.info('So should this')
-    logging.warning('And this, too')
-    logging.error('And non-ASCII stuff, too, like Øresund and Malmö')
-
-    pdb.set_trace()
-    logFilePathAndName = f"{dirName}/{logFileName}.log"
-    pdb.set_trace()
-    fileName = f"{logFileName}.log"
-    pdb.set_trace()
-    print(logFilePathAndName)
-    logging.basicConfig(filename=logFilePathAndName,
+    logDir = Path(dirName)
+    logFile = Path(f"{logFileName}.log")
+    
+    # create log directory if not exists
+    try:
+        logDir.mkdir(parents=True, exist_ok=True)
+    except:
+        msg = "could not create log directory"
+        return msg
+    
+    # create log file and set basic logging config
+    logFilePathAndName = logDir / logFile
+    try:
+        logging.basicConfig(filename=logFilePathAndName,
                          format='%(levelname)s[%(asctime)s] - %(module)s: %(message)s',
                          datefmt='%Y/%m/%d %I:%M:%S %p',
                          filemode='a',
-                         level=logging.debug)
-    pdb.set_trace()
-    logging.debug('logging lib test for debug level is ok, running into level')
-    logging.info('logging lib test for info level is ok, running into warining level')
-    print("b")
-    # New paragraph
-    with open(logFilePathAndName, 'a') as l:
-        # find the calling module
-        frm = inspect.stack()[1] # the call's frame: filepath, code line, etc
-        mod = inspect.getmodule(frm[0])
-        callingModule = inspect.getmodulename(mod.__file__)
+                         level=logging.DEBUG)
+    except:
+        msg = "could not apply basiCofig to logging service"
+        return msg
+    
+    try:
+        # Write a headline to the log file, asigning enableLogs's client file
+        with open(logFilePathAndName, 'a') as l:
+            # find the calling module
+            frm = inspect.stack()[1] # the call's frame: filepath, code line, etc
+            mod = inspect.getmodule(frm[0])
+            callingModule = inspect.getmodulename(mod.__file__)
 
-        # append first line of the process
-        l.write(f"\n -- Logger enabled by {callingModule}.py --\n")
-
-    logging.debug('logging lib test for debug level is ok, running into level')
-    logging.info('logging lib test for info level is ok, running into warining level')
-    print("c")
-    # logging.warning('logging lib test for warning level is ok, running into warning level')
-    # logging.error('logging lib test for error level is ok, running into critical level')
-    # logging.critical('logging lib test for critical level is ok, all levels have been tested')
+            # append first line of the process
+            l.write(f"\n -- Logger enabled by {callingModule}.py --\n")
+    except:
+        msg = "could not apply basiCofig to logging service"
+        return msg
+    
+    try:
+        logging.debug('logging lib test for debug level is ok, running into level')
+        logging.info('logging lib test for info level is ok, running into warining level')
+        logging.warning('logging lib test for warning level is ok, running into warning level')
+        logging.error('logging lib test for error level is ok, running into critical level')
+        logging.critical('logging lib test for critical level is ok, all levels have been tested')
+        logging.info('------------------- All logging levels successfully tested -------------------')
+    except:
+        msg = "failed writing logs to log file"
+        return msg
