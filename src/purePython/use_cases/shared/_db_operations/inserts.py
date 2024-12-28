4148fde9dcc5c56cc7e456c771a40095fc4539b8
Gabriel Ribeiro <gabriribeiro5@live.com>
2024-12-28 03:49:26 -0300

    feat(handle_post_unit_configurations): create sharable module

--- src/purePython/use_cases/shared/_db_operations/inserts.py
+++ src/purePython/use_cases/shared/_db_operations/inserts.py
@@ -31,12 +31,12 @@ class AgentTables(I_BaseHandler):
 class AgentInputTables(I_BaseHandler):
 
     # Handlers para as rotas
-    def handle_delete_some_data(self):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        response = {"data": self.data_store}
-        self.wfile.write(json.dumps(response).encode())
+    def handle_post_unit_configurations(self,
+                                        unit_ids: list = None,
+                                        evironment_category: list = None,
+                                        customer_id: list = None,
+                                        unit_location: list = None):
+        pass
 
 # raise warnings if db_operations is accessed outside of the intended scope
 if __name__ != "__main__" and "use_cases" not in __file__:  
