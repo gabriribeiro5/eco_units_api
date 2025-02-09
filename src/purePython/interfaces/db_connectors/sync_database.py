3700616966683434c036907f6cd0e683234e1fd8
Gabriel Ribeiro <gabriribeiro5@live.com>
2025-02-09 18:35:08 -0300

    fix(I_BaseDB): change file name, add logger and add method create_database

--- src/purePython/interfaces/db_connectors/sync_database.py
+++ src/purePython/interfaces/db_connectors/sync_database.py
@@ -1,21 +1,24 @@
 from utils.definitions import Definitions
+from utils.logSetup import log_running_and_done
 
 class I_BaseDB():
     def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         self.definitions = Definitions()
+        
         self.script_schema_ecosystem_db = self._load_sql(self.definitions._DB_SCRIPTS_DIR / "_0_schema_ecosystem_db.sql")
         self.script_insert_into_index_tables = self._load_sql(self.definitions._DB_SCRIPTS_DIR / "_1_insert_into_index_tables.sql")
         self.script_insert_into_agent_tables = self._load_sql(self.definitions._DB_SCRIPTS_DIR / "_2_insert_into_agent_tables.sql")
         self.script_insert_into_agent_input_tables = self._load_sql(self.definitions._DB_SCRIPTS_DIR / "_3_insert_into_agent_input_tables.sql")
-        
+
+        # This should not exist
         self.drop_table_query = "DROP TABLE IF EXISTS (%s)"
         self.insert_query = "INSERT INTO %s (%s) VALUES (%s)"
         self.select_query = "SELECT * FROM %s WHERE id = %s"
         self.delete_query = "DELETE FROM %s WHERE %s LIMIT %s"
         self.update_query = "UPDATE %s SET (%s) = %s WHERE %s = %s"
-        
-        super().__init__(*args, **kwargs)
 
+    @log_running_and_done
     def _load_sql(self, filename):
         '''
         Open and read the file as a single buffer
@@ -26,8 +29,17 @@ class I_BaseDB():
 
         return sqlScript
 
-    def _executeScriptsFromFile(self, sqlScript):
+    def _executeScript(self):
+        # This method must be implemented where the connection library is called
         raise NotImplementedError()
     
     def _connect():
+        # This method must be implemented where the connection library is called
         raise NotImplementedError()
+
+    @log_running_and_done
+    def create_database(self):
+        try:
+            self._executeScript(self.script_schema_ecosystem_db)
+        except Exception as e:
+            raise e
